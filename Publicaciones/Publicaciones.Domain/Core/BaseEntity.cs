62435579ccdf4481ef70a14743a6ea5144776775
Notmisa09 <125936102+Notmisa09@users.noreply.github.com>
2023-09-25 23:44:51 -0400

    Estructura inicial (Domain.Infrastructure)

---
+++ Publicaciones/Publicaciones.Domain/Core/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Publicaciones.Domain.Core
+{
+    public abstract class BaseEntity
+    {
+        public int IdCreationUser { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public int? IdModifiedUser {  get; set; }
+        public int? IdDeletedUser { get; set; }
+        public DateTime? DeletedDate {  get; set; }
+        public bool Deleted { get; set; }  
+    }
+}
