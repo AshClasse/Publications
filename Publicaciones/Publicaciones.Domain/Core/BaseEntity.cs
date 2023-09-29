e55c5bcbd0c8d6186de7fb776f19741a5558edfe
Ashley Classe Ripol <20222004@itla.edu.do>
2023-09-29 09:46:07 -0400

    Main Updated

--- Publicaciones/Publicaciones.Domain/Core/BaseEntity.cs
+++ Publicaciones/Publicaciones.Domain/Core/BaseEntity.cs
@@ -8,8 +8,8 @@ namespace Publicaciones.Domain.Core
     {
         public int IDCreationUser { get; set; }
         public DateTime CreationDate { get; set; }
+        public int? IDModifiedUser { get; set; }        
         public DateTime? ModifiedDate { get; set; }
-        public int? IDModifiedUser { get; set; }
         public int? IDDeletedUser { get; set; }
         public DateTime? DeletedDate { get; set; }
         public bool Deleted { get; set; }
