48549bd99554973529093325eef7b3443882bcc7
Ashley Classe Ripol <20222004@itla.edu.do>
2023-09-29 09:37:31 -0400

    Main Updated

---
+++ Publicaciones/Publicaciones.Domain/Repository/IBaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Publicaciones.Domain.Repository
+{
+ 
+    public interface IBaseRepository<TEntity> where TEntity : class
+    {
+        void Save(TEntity entity);
+        void Update(TEntity entity);
+        void Remove(TEntity entity);
+        List<TEntity> GetEntities();
+        TEntity GetEntityByID(int Id);
+    }
+}
+
