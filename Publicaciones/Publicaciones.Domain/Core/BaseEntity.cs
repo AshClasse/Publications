e12f770e915395b926d371a50b0f127eabe07c8e
RachelyPerez <68482998+rachelyperez31@users.noreply.github.com>
2023-10-06 23:24:22 -0400

    Adecuando a las convenciones establecidas.

--- Publicaciones/Publicaciones.Domain/Core/BaseEntity.cs
+++ Publicaciones/Publicaciones.Domain/Core/BaseEntity.cs
@@ -10,7 +10,7 @@ namespace Publicaciones.Domain.Core
         public DateTime CreationDate { get; set; }
         public int? IDModifiedUser { get; set; }
         public DateTime? ModifiedDate { get; set; }
-        public int? DeletedUser { get; set; }
+        public int? IDDeletedUser { get; set; }
         public DateTime? DeletedDate { get; set; }
         public bool Deleted { get; set; }
     }
