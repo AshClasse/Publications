6b21347f18c073911ffea1494412628e66458c34
RachelyPerez <68482998+rachelyperez31@users.noreply.github.com>
2023-10-08 12:53:20 -0400

    Definiendo los métodos exclusivos de cada entidad.

---
+++ Publicaciones/Publicaciones.Infrastructure/Interfaces/IDiscountRepository.cs
@@ -0,0 +1,13 @@
+﻿using Publicaciones.Domain.Entities;
+using Publicaciones.Domain.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Publicaciones.Infrastructure.Interfaces
+{
+    public interface IDiscountRepository : IBaseRepository<Discount>
+    {
+        List<Discount> GetDiscountsByStore(string storeID);
+    }
+}
---
+++ Publicaciones/Publicaciones.Infrastructure/Interfaces/ISaleRepository.cs
@@ -0,0 +1,15 @@
+﻿using Publicaciones.Domain.Entities;
+using Publicaciones.Domain.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Publicaciones.Infrastructure.Interfaces
+{
+    public interface ISaleRepository : IBaseRepository<Sale>
+    {
+        List<Sale> GetSalesByStore(string storeID);
+        List<Sale> GetSalesByTitle(string titleID);
+        List<Sale> GetSalesByOrdNum(string ordNum);
+    }
+}
