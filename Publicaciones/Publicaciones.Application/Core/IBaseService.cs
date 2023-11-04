700cc8ec9a6ef2e89604e991edec8d46c39863d0
G3remy <119114804+G3remy@users.noreply.github.com>
2023-11-03 23:43:52 -0400

    Update IBaseService.cs

--- Publicaciones/Publicaciones.Application/Core/IBaseService.cs
+++ Publicaciones/Publicaciones.Application/Core/IBaseService.cs
@@ -4,7 +4,12 @@ using System.Text;
 
 namespace Publicaciones.Application.Core
 {
-	internal interface IBaseService
+	internal interface IBaseService<TDtoAdd, TDtoUpdate, TDtoRemove>
 	{
+		ServiceResult GetAll();
+		ServiceResult GetById(int Id);
+		ServiceResult Save();
+  		ServiceResult Update(TDtoIpdate dtoUpdate);
+    		ServiceResult Remove(TDtoRemove dtoRemove);
 	}
 }
