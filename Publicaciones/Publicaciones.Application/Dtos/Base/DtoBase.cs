c73184a23d94962ab06d583bf75319c269fb1cb0
G3remy <119114804+G3remy@users.noreply.github.com>
2023-11-04 00:00:06 -0400

    Update DtoBase.cs

--- Publicaciones/Publicaciones.Application/Dtos/Base/DtoBase.cs
+++ Publicaciones/Publicaciones.Application/Dtos/Base/DtoBase.cs
@@ -4,7 +4,9 @@ using System.Text;
 
 namespace Publicaciones.Application.Dtos.Base
 {
-	internal class DtoBase
+	public class DtoBase
 	{
+		public int ChangeUser { get; set; }
+        	public DateTime ChangeDate { get; set; }
 	}
 }
